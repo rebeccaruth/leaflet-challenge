e2afb199a55f6a07a81358f6f48fbfae101a6832
Rebecca Ruth <Ruth0506@live.missouristate.edu>
2020-12-03 10:55:00 -0600

    tutor 2

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,21 +1,21 @@
-//import json
-json_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
 
 // Create the tile layer that will be the background of our map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  tileSize: 512,
   maxZoom: 18,
-  id: "light-v10",
-  accessToken: API_KEY
+  id: "mapbox/light-v10",
+  accessToken: "pk.eyJ1IjoiYmVja3lydXRoIiwiYSI6ImNraHdvYnoyYzA0OXAzMnFzc3BqZXkxbzEifQ.Y_TZdJGHz7-rYoMhOCBwNg"
 });
 
 //map
 var map = L.map("mapid", {
-    center: [40, -90],
+    center: [40.7, -94.5],
     zoom: 3,
 });
 lightmap.addTo(map);
 
+var json_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 d3.json(json_url, function(data){
     function styleMap (feature){
         return {
@@ -28,6 +28,7 @@ d3.json(json_url, function(data){
             weight: .5
         };
     }
+    
     function getColor(depth){
         switch(true){
             case depth > 90:
@@ -44,12 +45,14 @@ d3.json(json_url, function(data){
                 return "#98ee00";
         }
     }
+  
     function getRadius(magnitude){
         if (magnitude === 0){
             return 1;
         }
         return magnitude*4;
     }
+    
     L.geoJson(data, {
         pointToLayer: function (feature, latlng){
             return L.circleMarker(latlng);
@@ -59,9 +62,9 @@ d3.json(json_url, function(data){
             layer.bindPopup(
                 "magnitude: "
                 + feature.properties.mag
-                + "<br> depth: "
+                + "<br>depth: "
                 + feature.geometry.coordinates[2]
-                + "<br> location: "
+                + "<br>location: "
                 + feature.properties.place
             );
         }
@@ -88,7 +91,7 @@ d3.json(json_url, function(data){
             "#ea2c2c"
         ];
         for (var i = 0; i<depths.length; i++){
-            div.innerHTML += "<i style = 'background: "+ colors[i] + "'></i>"
+            div.innerHTML += "<i style = 'background:"+ colors[i] + "'></i>"
             + depths[i] + (depths [i+1] ? "&ndash;"+ depths[i+1]+ "<br>":"+");
         }
         return div;
